1c64e64d3ea152b91830f9a44cf0e51fdecacbc8
Roger <rogersoleramon@gmail.com>
2024-03-04 09:51:17 -0700

    Adding html, css and script

---
+++ Documents/HTML/test11/script.js
@@ -0,0 +1,80 @@
+function popup1(){
+    var popup1 = document.getElementById("popup1");
+    if(popup1.style.visibility == 'visible'){
+        popup1.style.visibility = 'hidden';
+    } else {
+        popup1.style.visibility = 'visible';
+    }
+}
+
+function popup2(){
+    var popup2 = document.getElementById("popup2");
+    if(popup2.style.visibility == 'visible'){
+        popup2.style.visibility = 'hidden';
+    } else {
+        popup2.style.visibility = 'visible';
+    }
+}
+
+function popup3(){
+    var popup3 = document.getElementById("popup3");
+    if(popup3.style.visibility == 'visible'){
+        popup3.style.visibility = 'hidden';
+    } else {
+        popup3.style.visibility = 'visible';
+    }
+}
+
+function popup4(){
+    var popup4 = document.getElementById("popup4");
+    if(popup4.style.visibility == 'visible'){
+        popup4.style.visibility = 'hidden';
+    } else {
+        popup4.style.visibility = 'visible';
+    }
+}
+
+function popup5(){
+    var popup5 = document.getElementById("popup5");
+    if(popup5.style.visibility == 'visible'){
+        popup5.style.visibility = 'hidden';
+    } else {
+        popup5.style.visibility = 'visible';
+    }
+}
+
+function popup6(){
+    var popup6 = document.getElementById("popup6");
+    if(popup6.style.visibility == 'visible'){
+        popup6.style.visibility = 'hidden';
+    } else {
+        popup6.style.visibility = 'visible';
+    }
+}
+
+function popup7(){
+    var popup7 = document.getElementById("popup7");
+    if(popup7.style.visibility == 'visible'){
+        popup7.style.visibility = 'hidden';
+    } else {
+        popup7.style.visibility = 'visible';
+    }
+}
+
+function popup8(){
+    var popup8 = document.getElementById("popup8");
+    if(popup8.style.visibility == 'visible'){
+        popup8.style.visibility = 'hidden';
+    } else {
+        popup8.style.visibility = 'visible';
+    }
+}
+
+function popup9(){
+    var popup9 = document.getElementById("popup9");
+    if(popup9.style.visibility == 'visible'){
+        popup9.style.visibility = 'hidden';
+    } else {
+        popup9.style.visibility = 'visible';
+    }
+}
